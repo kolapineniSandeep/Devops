b5ece954b358404cf3498c30af59f08077b3642a
kolapineniSandeep <sandeepkolapineni@gmail.com>
2022-02-06 21:02:45 -0500

    Update MvcConfig.java
    Test commit

--- ravi_demo/src/main/java/com/example/demo/MvcConfig.java
+++ ravi_demo/src/main/java/com/example/demo/MvcConfig.java
@@ -10,5 +10,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * 
  * @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
  * registry.addResourceHandler("/**")
- * .addResourceLocations("resources/static/"); } }
+ * .addResourceLocations("resources/static/"); }  }
  */
